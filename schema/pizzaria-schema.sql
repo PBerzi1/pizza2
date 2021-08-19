cd9bcd7f1f506960dadd5c4ec28fcdb709e75efb
PBerzi1 <pberzi74@gmail.com>
2021-08-19 11:31:00 +0100

    changes to files

--- schema/pizzaria-schema.sql
+++ schema/pizzaria-schema.sql
@@ -2,25 +2,32 @@ CREATE DATABASE pizza2;
 USE pizza2;
 
 
-CREATE TABLE <table name> (
-	<column_name1> <data_type> VARCHAR,
-	<column_name2> <data_type> UNIQUE NOT NULL,
-	PRIMARY KEY (<name_of_primary_column>)
-);
-
-
-CREATE TABLE pizzas (
-	pizza_id INT AUTO_INCREMENT, 
+CREATE TABLE pizzas(
+	pizza_id INT AUTO_INCREMENT,
 	pizza_type VARCHAR(40) NOT NULL,
 	price DECIMAL NOT NULL,
 	vegetarian BOOLEAN NOT NULL,
-	PRIMARY KEY (pizza_id)	
+	PRIMARY KEY (pizza_id)
 );
 
-CREATE TABLE orders (
+
+CREATE TABLE customer(
+	customer_id INT,
+	customer_name VARCHAR(50),
+	customer_address VARCHAR(70),
+	new_member BOOLEAN,
+	PRIMARY KEY (customer_id)
+);
+
+
+CREATE TABLE orders(
 	order_id INT AUTO_INCREMENT,
+	customer_id INT,
 	fk_pizza_id INT,
-	date_booking DATETIME NOT NULL, 
+	fk_customer_id INT,
+	date_booking DATETIME NOT NULL,
 	PRIMARY KEY (order_id),
-	FOREIGN KEY (fk_pizza_id) REFERENCES pizzas(pizza_id)
+	FOREIGN KEY (fk_pizza_id) REFERENCES pizzas(pizza_id),
+	FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id)
+	
 );
